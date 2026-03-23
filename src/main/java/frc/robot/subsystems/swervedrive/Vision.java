268f538c535b860238e25ed4b67d65c3740e2197
frc8243 <jmhsrobotics@johnmarshallit.com>
2026-03-23 18:44:18 -0400

    final vision file

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import swervelib.SwerveDrive;
 
 public class Vision {
@@ -40,8 +41,16 @@ public class Vision {
         LimelightHelpers.SetRobotOrientation(
                 limelightName, drive.getYaw().getDegrees(), 0, 0, 0, 0, 0);
 
-        // reject if spinning too fast (> 720 deg/sec = 2 rot/sec)
+        // reject if spinning too fast (> 2 rot/sec)
         if (Math.abs(drive.getRobotVelocity().omegaRadiansPerSecond) > (2 * Math.PI * 2)) return;
+
+        double linearSpeed =
+                Math.hypot(
+                        drive.getRobotVelocity().vxMetersPerSecond,
+                        drive.getRobotVelocity().vyMetersPerSecond);
+
+        //reject if driving to0 fast, > 80% of robot speed! 
+        if (linearSpeed > 0.8 * drive.getMaximumChassisVelocity()) return;
 
         var est = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
         if (est == null) return;
@@ -51,11 +60,19 @@ public class Vision {
         // publish pose to NT for AdvantageScope/Shuffleboard
         posePublisher.set(est.pose);
 
+        StringBuilder tagIDs = new StringBuilder();
+        if (est.rawFiducials != null) {
+            for (int i = 0; i < est.rawFiducials.length; i++) {
+                if (i > 0) tagIDs.append(", ");
+                tagIDs.append(est.rawFiducials[i].id);
+            }
+        }
+        SmartDashboard.putString("Vision/TagIDs", tagIDs.toString());
         // use Limelight's own stddevs instead of hardcoded values
         // layout: [MT1x, MT1y, MT1z, MT1roll, MT1pitch, MT1yaw, MT2x, MT2y, MT2z, MT2roll,
         // MT2pitch, MT2yaw]
         var stddevs = LimelightHelpers.getLimelightNTDoubleArray(limelightHostname, "stddevs");
-
+        if (stddevs == null || stddevs.length < 8) return;
         double timestamp =
                 Timer.getFPGATimestamp() - (est.latency_capture + est.latency_pipeline) / 1000.0;
 
